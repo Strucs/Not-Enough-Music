14febac92a1a147cce8b7b6c968bdffa66436edf
nath54 <cerisara.nathan@gmail.com>
2025-03-31 11:24:08 +0200

    Debug Training

--- lib_training.py
+++ lib_training.py
@@ -6,6 +6,7 @@ import os
 from datetime import datetime
 #
 import torch
+from torch import Tensor
 from torch import optim
 from torch import nn
 #
@@ -111,15 +112,18 @@ def train_simple_epochs_loop(
 
             #
             x_input = x_batch[ (i*batch_parallel_calcul) : ((i+1)*batch_parallel_calcul) ].to(dtype=torch.float32)
-            y_input = y_batch[ (i*batch_parallel_calcul) : ((i+1)*batch_parallel_calcul) ].to(dtype=torch.float32)
+            y_input = y_batch[ (i*batch_parallel_calcul) : ((i+1)*batch_parallel_calcul) ]
             #
-            y_input = y_input.squeeze().to(dtype=torch.int64)
+            if len(y_input.shape) > 1:
+                y_input = y_input.squeeze()
+            if len(list(y_input.shape)) < 1:
+                y_input = Tensor( [y_input] )
+
+            #
+            y_input = y_input.to(dtype=torch.int64)
 
             #
             pred: Tensor = model(x_input).to(dtype=torch.float32)
-
-            #
-            print(f"DEBUG | x_input.shape = {x_input.shape}\n | y_input.shape = {y_input.shape}\n | pred.shape = {pred.shape}")
 
             #
             loss = loss_fn(pred, y_input)
