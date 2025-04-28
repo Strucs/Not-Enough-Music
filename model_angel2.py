1ef286e13a967a5c792cb8778fe9129f241fbbc6
Strucs <unstrucs@outlook.fr>
2025-04-28 10:42:23 +0200

    update

---
+++ model_angel2.py
@@ -0,0 +1,41 @@
+
+import torch
+import torch.nn as nn
+
+
+
+
+# Définition du modèle CNN 1D
+class SimpleCNN1D(nn.Module):
+    def __init__(self, input_channels, num_classes):
+        super(SimpleCNN1D, self).__init__()
+
+        self.conv1 = nn.Conv1d(in_channels=input_channels, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv1d(in_channels=64, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv1d(in_channels=32, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.fc = nn.Linear(16 * (675808 // (2 ** 5) ), num_classes)
+
+    def forward(self, x):
+        x = x.unsqueeze(1)
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.conv4(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.conv5(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
