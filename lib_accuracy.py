90f83f5be7b02fea192caf9800a1ff24a0ed4a37
Gaxali <gaxaliz@gmail.com>
2025-04-28 10:42:20 +0200

    to device accuracy

--- lib_accuracy.py
+++ lib_accuracy.py
@@ -7,13 +7,18 @@ from tqdm import tqdm
 #
 from lib_dataset import Dataset
 
-
+from lib_device import get_device
 
 #
 def calculate_accuracy(dataset: Dataset, model: nn.Module) -> None:
 
+    model = model.to(get_device())
+
     #
     x_test, y_test = dataset.get_full_test()
+
+    x_test = x_test.to(get_device())
+    y_test = y_test.to(get_device())
 
     #
     tot: float = 0
@@ -24,7 +29,7 @@ def calculate_accuracy(dataset: Dataset, model: nn.Module) -> None:
     #
     for i in tqdm(range(len(x_test))):
         #
-        pred = model(x_test[i].unsqueeze(0))
+        pred = model(x_test[i].unsqueeze(0)).to(get_device())
 
         #
         idx_pred = torch.argmax( pred, dim = -1 )
